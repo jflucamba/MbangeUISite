d89b69b704df34db3cd02a1b6788bd8b1e4b5c42
José Lucamba <jflucamba@gmail.com>
2021-02-07 07:26:51 +0100

    criação e estruturação do projecto

---
+++ mbange-site/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>
+      
+      <NavBarComponent msg="Welcome to Your Vue.js App"/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import NavBarComponent from './components/NavBarComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBarComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
