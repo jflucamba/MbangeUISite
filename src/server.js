e6f5a79d9c5e4974fd83a2c113e1040422e0c4c1
José Lucamba <jflucamba@gmail.com>
2021-02-14 22:39:01 +0100

    Actualização de server

--- src/server.js
+++ src/server.js
@@ -3,8 +3,12 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
+//we are configuring dist to serve site files
 app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 5000
+// this * route is to serve project on different page routes except root `/`
+//app.get(/.*/, function (req, res) {
+ //res.sendFile(path.join(__dirname, '/dist/index.html'))
+//})
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
